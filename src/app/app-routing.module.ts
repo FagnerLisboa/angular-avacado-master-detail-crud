9e5d687b94b3e0fcc60bc15f3080d10fa33ae15d
FagnerLisboa <fnlisboa2016@gmail.com>
2023-08-03 21:44:30 -0300

    adding base configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) },
+
+  { path: '', redirectTo: '/reports', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
